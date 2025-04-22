6dabc3841f244d80850c3e539a37a73b4718e913
AndrewFudge <fudge@gmx.co.uk>
2025-04-22 12:06:00 +0100

    formatted for PEP8

--- src/util/octopus_api.py
+++ src/util/octopus_api.py
@@ -1,25 +1,35 @@
 import requests
-from pprint import pprint
+
 
 def octopus_api_get(date_start, date_end):
+    """util to get energy prices from Octopus
+    returns a dictionary with 30 min windows
+    use date format ZULU i.e
+    `2020-03-29T00:00Z`
+
+    Args:
+        date_start (str): date format 'YYYY-MM-DDTHH:mmZ'
+        date_end (str): date format 'YYYY-MM-DDTHH:mmZ'
+
+    Returns:
+        dict: returns a dict with start times as keys and a dict as value
+            inside are times and values
+    """
     # dates in format of '2020-03-29T00:00Z' '2020-03-29T02:29Z'
     # Z is zulu time
     # should return data every 30 mins
     # tarrif is currently hard coded to AGILE-24-10-01
     # region currently hard coded using tarrif_url
-    
-    base_url = f'https://api.octopus.energy/v1/products/'
-    tarrif_url = f'AGILE-24-10-01'
-    region_url = f'/electricity-tariffs/E-1R-{tarrif_url}-A/standard-unit-rates/'
+
+    base_url = 'https://api.octopus.energy/v1/products/'
+    tarrif_url = 'AGILE-24-10-01'
+    region_url = f'/electricity-tariffs/E-1R-{tarrif_url}-A/'\
+        'standard-unit-rates/'
     period_url = f'?period_from={date_start}&period_to={date_end}'
     full_url = base_url + tarrif_url + region_url + period_url
-    
-    # print(full_url)
 
     requested_data = requests.get(full_url)
     output_dict = requested_data.json()
-
-    # pprint(output_dict)
 
     time_data_dict = {}
 
